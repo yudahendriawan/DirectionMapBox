795617ad729bd2a4289246bf8fffa6a713f48190
yudahendriawan <yudahendriawan007@gmail.com>
2020-05-19 22:46:25 +0700

    login activity + error handled

--- app/src/main/java/com/yudahendriawan/ProjectTugasAkhir/EmailActivity.java
+++ app/src/main/java/com/yudahendriawan/ProjectTugasAkhir/EmailActivity.java
@@ -2,10 +2,12 @@ package com.yudahendriawan.ProjectTugasAkhir;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -29,6 +31,7 @@ public class EmailActivity extends AppCompatActivity {
     private TextInputLayout textInputEmail;
     private TextInputLayout textInputUsername;
     private TextInputLayout textInputPassword;
+    private Button confirm;
 
     String emailInput;
     String usernameInput;
@@ -60,10 +63,12 @@ public class EmailActivity extends AppCompatActivity {
 
         if (emailInput.isEmpty()) {
             textInputEmail.setError("Field can't be empty");
+            textInputEmail.requestFocus();
             return false;
             //regular expression (ReGex)
         } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
             textInputEmail.setError("Please enter a valid email address");
+            textInputEmail.requestFocus();
             return false;
         } else {
             textInputEmail.setError(null);
@@ -77,9 +82,11 @@ public class EmailActivity extends AppCompatActivity {
 
         if (usernameInput.isEmpty()) {
             textInputUsername.setError("Field can't be empty");
+            textInputUsername.requestFocus();
             return false;
         } else if (usernameInput.length() > 15) {
             textInputUsername.setError("Username too long");
+            textInputUsername.requestFocus();
             return false;
         } else {
             textInputUsername.setError(null);
@@ -93,9 +100,12 @@ public class EmailActivity extends AppCompatActivity {
 
         if (passwordInput.isEmpty()) {
             textInputPassword.setError("Field can't be empty");
+            textInputPassword.requestFocus();
             return false;
+            //regex check
         } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
-            textInputPassword.setError("Password too weak");
+            textInputPassword.setError("Password must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character");
+            textInputPassword.requestFocus();
             return false;
         } else {
             textInputPassword.setError(null);
@@ -108,13 +118,31 @@ public class EmailActivity extends AppCompatActivity {
 
         if (!validateEmail() | !validateUsername() | !validatePassword()) {
             return;
-        } else if (emailInput.equals(email) && usernameInput.equals(username) && passwordInput.equals(password)) {
-            String input = "Email:" + textInputEmail.getEditText().getText().toString();
-            input += "\n";
-            input += "Username :" + textInputUsername.getEditText().getText().toString();
-            input += "\n";
-            input += "Password : " + textInputPassword.getEditText().getText().toString();
-            Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+        } else if (validateEmail() | validateUsername() | validatePassword()) {
+            if (!emailInput.equals(email)) {
+                textInputEmail.setError("Wrong email");
+                textInputEmail.requestFocus();
+                return;
+            }
+            if (!usernameInput.equals(username)) {
+                textInputUsername.setError("Wrong username");
+                textInputUsername.requestFocus();
+                return;
+            }
+            if (!passwordInput.equals(password)) {
+                textInputPassword.setError("Wrong password");
+                textInputPassword.requestFocus();
+                return;
+            } else {
+                String input = "Email:" + textInputEmail.getEditText().getText().toString();
+                input += "\n";
+                input += "Username :" + textInputUsername.getEditText().getText().toString();
+                input += "\n";
+                input += "Password : " + textInputPassword.getEditText().getText().toString();
+                Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, MenuActivity.class);
+                startActivity(intent);
+            }
         }
 
 
